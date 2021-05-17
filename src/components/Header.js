1060e4d5a7a2868b10f346291e5f8363dc85338e
Mahmoud Saadeh <mahmoud.saadeh998@gmail.com>
2021-05-17 16:18:11 +0300

    added votes button and state and bootstrab

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class Header extends Component {
     render() {
         return (
-            <div>
+            <div className='header'>
                 <h1>Gallery of horns</h1>
             </div>
         )
--- src/components/HornedBeasts.js
+++ src/components/HornedBeasts.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Card } from 'react-bootstrap';
 export default class HornedBeasts extends Component {
+  constructor(props){
+    super(props);
+    this.state={votes: 0,};
+  }
+
+  voteStateHandler = ()=>{
+    this.setState({
+      votes: this.state.votes + 1
+    })
+    console.log(this.state.votes);
+  }
   render() {
     return (
       <div>
-        {console.log(this)}
-        <h2>{this.props.title}</h2>
+        {/* <h2>{this.props.title}</h2>
         <img
           src={this.props.src}
           alt={this.props.title}
@@ -13,6 +24,21 @@ export default class HornedBeasts extends Component {
           className="image-beast"
         />
         <p>{this.props.description}</p>
+        <p>❤️ {this.state.votes} {this.state.votes > 1 ? 'Votes' : 'Vote'}</p>
+        <button onClick={this.voteStateHandler}>Vote</button> */}
+        <Card style={{ width: '18rem', margin:'0px 0px 30px 0px' }}>
+          <Card.Img variant="top" src={this.props.src} />
+          <Card.Body>
+            <Card.Title>{this.props.title}</Card.Title>
+            <Card.Text>
+              {this.props.description}
+            </Card.Text>
+            <Card.Text>
+              ❤️ {this.state.votes} {this.state.votes > 1 ? 'Votes' : 'Vote'}
+            </Card.Text>
+            <Button onClick={this.voteStateHandler} variant="primary">Vote</Button>
+          </Card.Body>
+        </Card>
       </div>
     );
   }
