8634863cc7f61d3e91d33d5e9a5ffd06f4e598b2
Mahmoud Saadeh <mahmoud.saadeh998@gmail.com>
2021-05-18 14:45:58 +0300

    modal appears after clicking on the card

--- src/App.js
+++ src/App.js
@@ -1,14 +1,54 @@
-import React, { Component } from 'react'
-import Footer from './components/Footer'
-import Header from './components/Header'
-import Main from './components/Main'
+import React, { Component } from 'react';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Main from './components/Main';
+import SelectBeast from './components/SelectBeast';
+import Beasts from "./components/BeastsData.json"
 
 export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      Beasts: Beasts,
+      showHide: false,
+      src:'',
+      title:'',
+      description:'',
+      votes:0,
+    };
+
+  }
+  getDataHandler = (src,title,description,votes)=>{
+    this.setState({
+      src,
+      title,
+      description,
+      votes,
+    })
+  }
+  showModalHandler = ()=>{
+    this.setState({
+      showHide: true,
+    })
+  }
+  hideModalHandler = ()=>{
+    this.setState({
+      showHide: false,
+    })
+  }
   render() {
     return (
       <div>
         <Header/>
-        <Main/>
+        <Main Beasts={this.state.Beasts} showHandler={this.showModalHandler} getDataHandler={this.getDataHandler}/>
+        <SelectBeast 
+        closeHandler={this.hideModalHandler} 
+        showHide={this.state.showHide}
+        src={this.state.src}
+        title={this.state.title}
+        description={this.state.description}
+        votes={this.state.votes}
+        />
         <Footer/>
       </div>
     )
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react'
 export default class Footer extends Component {
     render() {
         return (
-            <div>
+            <footer>
                 <p>All right reserved &copy; | Mahmoud Saadeh</p>
-            </div>
+            </footer>
         )
     }
 }
